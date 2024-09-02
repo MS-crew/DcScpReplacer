a3936c13f80670cc74c8c7fb9e26ecdfbb2c33b8
MS-crew <mustafavesavas1@gmail.com>
2024-09-03 01:17:29 +0300

    Fix: Player.Left event handler multiple subscription prevention again & added ReplaceRolesfordcscp is null validation

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -9,7 +9,7 @@
     public class EventHandlers
     {
         private readonly Plugin plugin;
-        private bool isPlayerLeftHandlerAdded = false;
+        private bool isPlayerLeftHandlerAdded = true;
         public EventHandlers(Plugin plugin) => this.plugin = plugin;
         public void OnWaitingForPlayers()
         {
@@ -30,7 +30,15 @@
 
             if (!ev.Player.IsScp || ev.Player.Role == RoleTypeId.Scp0492)
                 return;
+
             Player? newscp = null;
+
+            if (Plugin.Instance.Config.ReplaceRolesfordcscp is null)
+            {
+                Log.Info($"replace_rolesfordcscp list is empty!Should be not!!!");
+                return;
+            }
+
             foreach (RoleTypeId role in Plugin.Instance.Config.ReplaceRolesfordcscp)
             {
                 newscp ??= Player.List.FirstOrDefault(x => x.Role == role);
@@ -42,8 +50,10 @@
 
             if (newscp == null)
                 return;
+
             DcScp dcplayer = new(ev.Player);
             dcplayer.NewScp(newscp);
+
             if (Plugin.Instance.Config.ScpReplaceAnnouncement)
             {
                 if (newscp.Role == RoleTypeId.Scp079)
@@ -54,6 +64,7 @@
                     Map.Broadcast(3,ScpReplaceMessage);
                 }    
             }
+
             Exiled.Events.Handlers.Map.AnnouncingScpTermination += ScpTerminationAnnouncement;
             newscp.Broadcast(Plugin.Instance.Translation.NowYouAreScp,true);
         }
